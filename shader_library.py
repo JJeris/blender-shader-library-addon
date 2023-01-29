0c1bdc7a31b9432792683262ccbdb904a276afff
Johans Justs Eris <johans.eris@r6vsk.lv>
2023-01-29 20:22:53 +0200

    Add glass nodes

--- shader_library.py
+++ shader_library.py
@@ -21,48 +21,80 @@ class ShaderMainPanel (bpy.types.Panel):
     bl_category = "Shader Library"    
    
    
-    def draw(self):
+    def draw(self, context): ## Draw needs both self and context, 2 args
+        """Draws the panel for the Shader Library addon"""
         layout = self.layout
+        row = layout.row()
+        
+        row.label(text = "Select a Shader to be added.")
+        row.operator("")
+        
+        
+        
+class SHADER_OT_DIAMOND(bpy.types.Operator):
+    """Create a Custom Operator for the Diamond Shader"""
+    bl_label = "Diamond" 
+    bl_idname = "shader.diamond.operator"
     
+    def execute(self, context):
+        """Creates a new shader called Diamond"""
+        
+        ##SET UP
+        
+        ## Creating a New Shader and calling it Diamond
+        material_diamond = bpy.data.materials.new(name = "Diamond")
+        ## Enabling Use Nodes
+        material_diamond.use_node = True
+        
+        ## Removes the automatically loaded "Principled BSDF" node
+        material_diamond.node_tree.nodes.remove(material_diamond.node_tree.nodes.get("Principled BSDF"))
+        
+        ## Create a reference to the Material Output
+        material_output = material_diamond.node_tree.nodes.get("Material Output")
+        ## Set location of node
+        material_output.location = (-400, 0) ##(x,y) coordinates
+    
+        ### GLASS NODES
+        
+        ## Adding Glass1 Node
+        glass1_node = material_diamond.node_tree.nodes.new(type = "ShaderNodeBsdfGlass")
+        ## Set location of node
+        glass1_node.location = (-600, 0)
+        ## Setting the Default Color
+        glass1_node.inputs[0].default_value = (1, 0, 0, 1) ## RGBA. Red
+        ## Setting the Default IOR Value
+        glass1_node.inputs[2].default_value = 1.446 ## IOR - Index of refraction
+        
+        ## Adding Glass2 Node
+        glass2_node = material_diamond.node_tree.nodes.new(type = "ShaderNodeBsdfGlass")
+        ## Set location of node
+        glass2_node.location = (-600,-150)
+        ## Setting the Default Color
+        glass2_node.inputs[0].default_value = (0, 1, 0, 1) ## RGBA. Red
+        ## Setting the Default IOR Value
+        glass2_node.inputs[2].default_value = 1.450 ## IOR - Index of refraction
+
+        ## Adding Glass3 Node
+        glass3_node = material_diamond.node_tree.nodes.new(type = "ShaderNodeBsdfGlass")
+        ## Set location of node
+        glass3_node.location = (-600,-300)
+        ## Setting the Default Color
+        glass3_node.inputs[0].default_value = (0, 0, 1, 1) ## RGBA. Red
+        ## Setting the Default IOR Value
+        glass3_node.inputs[2].default_value = 1.500 ## IOR - Index of refraction
+        
+        
+
+
+
+
+
+
 def register():
     bpy.utils.register_class(ShaderMainPanel)
+    
 def unregister():
     bpy.utils.unregister_class(ShaderMainPanel)
+
 if __name__ == "__main__":
     register()
-    
-# class HelloWorldPanel(bpy.types.Panel):
-#     """Creates a Panel in the Object properties window"""
-#     bl_label = "Hello World Panel"
-#     bl_idname = "OBJECT_PT_hello"
-#     bl_space_type = 'PROPERTIES'
-#     bl_region_type = 'WINDOW'
-#     bl_context = "object"
-
-#     def draw(self, context):
-#         layout = self.layout
-
-#         obj = context.object
-
-#         row = layout.row()
-#         row.label(text="Hello world!", icon='WORLD_DATA')
-
-#         row = layout.row()
-#         row.label(text="Active object is: " + obj.name)
-#         row = layout.row()
-#         row.prop(obj, "name")
-
-#         row = layout.row()
-#         row.operator("mesh.primitive_cube_add")
-
-
-# def register():
-#     bpy.utils.register_class(HelloWorldPanel)
-
-
-# def unregister():
-#     bpy.utils.unregister_class(HelloWorldPanel)
-
-
-# if __name__ == "__main__":
-#     register()
